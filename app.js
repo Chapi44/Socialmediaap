5fa7b457826219a1b1f0445f7d05bbc5a5c1c4c3
chapi44 <yeabsiraashenafi5@gmail.com>
2024-03-27 17:07:24 +0300

    modified

--- app.js
+++ app.js
@@ -54,6 +54,20 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/chat", chatRoutes);
 
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/root"));
+app.all("*", (req, res) => { 
+  res.status(404); 
+  if (req.accepts("html")) { 
+    res.sendFile(path.join(__dirname, "views", "404.html")); 
+  } else if (req.accepts("json")) { 
+    res.json({ message: "404 Not Found" }); 
+  } else { 
+    res.type("txt").send("404 Not Found"); 
+  } 
+});
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
